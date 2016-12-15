7dc2b5f82c5c9ab565fb8a7d36a601be31597fb6
1CameronT <c.toy@sjasd.ca>
2016-12-15 09:53:54 -0600

    Lab 6-1
    Completed lab 6-1

---
+++ src/DisplayName.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ * Name:Cameron
+ * Teacher:Mr.Hardman
+ * Assignment 6, Program 1
+ * Date last modified:December 8
+ */
+
+/**
+ * @author c.toy
+ *
+ */
+public class DisplayName {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		displayName();
+		displayBirthday();
+		displayFact();
+
+	}
+	
+	/**
+	 * displayName will display the the user name
+	 * 
+	 * @param There are no parameters.
+	 * @return Nothing will be returned.
+	 */
+	public static void displayName(){
+			
+		System.out.println("Your name is Cameron");
+		
+	}
+	
+	/**
+	 * displayBirthday will display the user birthday
+	 * 
+	 * @param There are no parameters.
+	 * @return Nothing will be returned.
+	 */
+	public static void displayBirthday(){
+		
+		System.out.println("Your birthday is April 27");
+		
+	}
+	
+	/**
+	 * displayFact will display a fact about the user
+	 * 
+	 * @param There are no parameters.
+	 * @return Nothing will be returned.
+	 */
+	public static void displayFact(){
+		
+		System.out.println("You favorite sport is basketball ");
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
